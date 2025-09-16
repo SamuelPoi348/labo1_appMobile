ca20fd4b42d9605fcd5c53582bedb96637133560
Samuel  Poirier <S.Poirier>
2025-09-16 11:31:16 -0400

    fin des import et des pip install je pense pour le labo04

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from app import app
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager, UserMixin
+
+# Config de Flask Login
+login = LoginManager(app)
+login.login_view = 'connexion' # Nom de la vue pour la page de connexion
+login.login_message = 'Veuillez vous connecter pour accéder à cette page.'
 
 app = Flask(__name__)
 app.config.from_object(Config)
--- app/models.py
+++ app/models.py
@@ -1,11 +1,24 @@
-from typing import Optional, List  
+from typing import Optional, List 
+from flask_sqlalchemy import SQLAlchemy 
 import sqlalchemy as sa 
 import sqlalchemy.orm as so 
 from datetime import datetime, timezone, date, time 
 import decimal 
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin
 
 
+db = SQLAlchemy()
+
+
+#is_authenticated
+#is_active
+#is_anonymous
+#get_id()
+#@login.user_loader
+#def load_user(id):
+# retourner l’utilisateur par son id
 class Utilisateur(db.Model):
     __tablename__ = 'utilisateur'
 
@@ -21,6 +34,14 @@ class Utilisateur(db.Model):
 
     def __repr__(self) -> str:
         return f"<Utilisateur {self.nom_utilisateur}>"
+    
+    def genere_mot_passe(self, mot_passe: str) -> None:
+
+        self.mot_passe_hash = generate_password_hash(mot_passe)
+
+    def verifier_mot_de_passe(self, mot_passe: str) -> bool:
+
+        return check_password_hash(self.mot_passe_hash,mot_passe)
 
 
 class Post(db.Model):
