4992492f010c42b3a929f7aad86aee7b55a37ffb
SamuelPoi348 <samanniep@gmail.com>
2025-10-03 10:09:59 -0400

    ajout des definition pour les followers et following

--- app/models.py
+++ app/models.py
@@ -50,6 +50,44 @@ class Utilisateur(db.Model, UserMixin):
         secondary=followers,primaryjoin=(followers.c.followed_id == id),
         secondaryjoin=(followers.c.follower_id == id),
         back_populates='following')
+    
+    def follow(self, utilisateur):
+        if not self.is_following(utilisateur):
+            self.following.append(utilisateur)
+    
+    def unfollow(self, utilisateur):
+        if self.is_following(utilisateur):
+            self.following.remove(utilisateur)
+    
+    def is_following(self, utilisateur):
+        query = self.following.select().where(Utilisateur.id == utilisateur.id)
+        return db.session.scalar(query) is not None
+    
+    def followers_count(self):
+        query = sa.select(sa.func.count()).select_from(
+            self.followers.select().subquery())
+        return db.session.scalar(query)
+    
+    def following_count(self):
+        query = sa.select(sa.func.count()).select_from(
+            self.following.select().subquery())
+        return db.session.scalar(query)
+    
+    def following_posts(self):
+       Author = so.aliased(Utilisateur)
+       Follower = so.aliased(followers)
+       return (
+           sa.select(Post)
+           .join(Post.author.of_type(Author))
+           .join(Author.followers.of_type(Follower), isouter=True)
+           .where(
+               sa.or_(
+                   Follower.c.follower_id == self.id,
+                   Post.id_utilisateur == self.id
+               ))
+               .group_by(Post)
+                .order_by(Post.timestamp.desc())
+       )
 
 
 class Post(db.Model):
