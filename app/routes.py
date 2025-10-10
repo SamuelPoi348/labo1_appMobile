94bf5d284270c4ee85f350e2c13bd29649b95d5f
SamuelPoi348 <samanniep@gmail.com>
2025-10-10 11:09:49 -0400

    ajout de la pagination pour index et explorer

--- app/routes.py
+++ app/routes.py
@@ -32,13 +32,13 @@ def index():
         return redirect(url_for('index'))
     
     if current_user.is_authenticated:
-        posts = db.session.scalars(
-            sa.select(Post)
-              .where(Post.id_utilisateur == current_user.id)
-              .order_by(Post.timestamp.desc())
-        ).all()
-        posts = db.session.scalars(current_user.following_posts()).all()
-        return render_template('index.html', utilisateur=current_user, posts=posts,form=form)
+       
+        page = request.args.get('page', 1, type=int)
+        posts = db.paginate(current_user.following_posts(), page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+
+        next_url = url_for('index', page=posts.next_num) if posts.has_next else None
+        prev_url = url_for('index', page=posts.prev_num) if posts.has_prev else None
+        return render_template('index.html', utilisateur=current_user, posts=posts.items,form=form, next_url=next_url, prev_url=prev_url)
     return render_template('index.html')
 
 @app.route('/connexion', methods=['GET', 'POST'])
@@ -192,7 +192,10 @@ def unfollow(nom_utilisateur):
 @app.route('/explore')
 @login_required
 def explore():
+  page = request.args.get('page', 1, type=int)
   query = sa.select(Post).order_by(Post.timestamp.desc())
-  posts = db.session.scalars(query).all()
-  return render_template('index.html', title="Explorer", posts=posts)
+  posts = db.paginate(query, page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+  next_url = url_for('explore', page=posts.next_num) if posts.has_next else None
+  prev_url = url_for('explore', page=posts.prev_num) if posts.has_prev else None
+  return render_template('index.html', title="Explorer", posts=posts.items,next_url=next_url, prev_url=prev_url)
 
