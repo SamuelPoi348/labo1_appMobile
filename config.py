140661d30a0428195060097748c959dd6566910f
Samuel  Poirier <S.Poirier>
2025-10-22 13:57:07 -0400

    ajout d'autres fix mais quand même bug

--- config.py
+++ config.py
@@ -7,10 +7,12 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False  
 
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+     # Configuration email (Gmail par exemple)
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.gmail.com'
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 587)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None or True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'one@gamil.com'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'mot_de_passe_ou_app_password'
+    MAIL_DEFAULT_SENDER = ('LaboApp', MAIL_USERNAME)
     ADMINS = ['one@gamil.com']
     POSTS_PER_PAGE = 3
